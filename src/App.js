662a795991b7b0270cd2844144f844086e2df0c5
suraj_122 <122suraj0@gmail.com>
2023-03-21 17:04:37 +0530

    add home, exchanges, news, currencies routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  HomePage,
+  Exchanges,
+  CryptoCurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
+import "./styles/styles.css";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Crypto World!</h1>
+    <div className="app">
+      <aside className="navbar">
+        <Navbar />
+      </aside>
+      <main className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<HomePage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<CryptoCurrencies />}
+              />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+
+        <footer className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Cryptoworld <br />
+            All Rights Reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exhanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </footer>
+      </main>
     </div>
   );
 }
